f522da75119cc05e285ab5a51160e0206ded7492
brendanbohn <brendanbohn@comcast.net>
2015-10-02 08:56:43 -0700

    homework w01d04 events lab commit

--- change/base.js
+++ change/base.js
@@ -2,6 +2,14 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  $("#time").text( Date.now() );
+	$('input.add').change( function() {
+		var left = $("input#left").val();
+		var right = $('input#right').val();
+		var leftVal = parseInt(left) || 0;
+		var rightVal = parseInt(right) || 0;
+		var total = leftVal + rightVal;
+		$('#total').val(total);
+	});
 
-})
+  $("#time").text( Date.now() );
+});
--- submit/base.js
+++ submit/base.js
@@ -1,7 +1,10 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+	$('form').click(function(e) {
+		e.preventDefault();
+	});
 
   $("#time").text( Date.now() );
 
-})
+});
